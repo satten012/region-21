ea1ea5833c8d2222d4e1394b6525049e59f1facf
unknown <essatenilas@gmail.com>
2023-10-28 23:40:53 +0300

    js new code

--- js/main.js
+++ js/main.js
@@ -674,3 +674,240 @@ HallOfFameStatistics.forEach((a,o)=>{
 
   })
 })
+
+/* ГУГЛ ДОК */
+/*
+function downloadHistoricalData () {
+
+function fetchYahooFinanceDataToGoogleSheet() {
+  var symbols = ["^GSPC", "^DJI", "^GDAXI", "^FTSE", "GC=F", "GC=F", "GC=F",  "USDJPY=X", "EURJPY=X", "GBPJPY=X", "CADJPY=X", "NZDJPY=X", "AUDJPY=X", "USDCHF=X", "EURCHF=X", "GBPCHF=X", "CADCHF=X", "NZDCHF=X", "AUDCHF=X", "USDMXN=X", "USO", "EURHUF=X", "UNG", "EURUSD=X", "GBPUSD=X", "AUDUSD=X", "NZDUSD=X", "USDCAD=X"];
+  var labels = ["US500", "DJI", "DAX", "FTSE", "XAUUSD", "XAUEUR", "XAUGBP", "USDJPY", "EURJPY", "GBPJPY", "CADJPY", "NZDJPY", "AUDJPY", "USDCHF", "EURCHF", "GBPCHF", "CADCHF", "NZDCHF", "AUDCHF", "USDMXN", "USO", "EURHUF", "UNG", "EURUSD", "GBPUSD", "AUDUSD", "NZDUSD", "USDCAD"];
+  var baseURL = "https://query1.finance.yahoo.com/v7/finance/download/";
+  var period1 = "0";
+  var period2 = "9999999999";
+  var interval = "1d";
+  var events = "history";
+  var queryString = "?period1=" + period1 + "&period2=" + period2 + "&interval=" + interval + "&events=" + events;
+
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = spreadsheet.getSheetByName("Data");
+
+  if (sheet == null) {
+    sheet = spreadsheet.insertSheet("Data");
+  }
+
+  var column = 1;
+
+  for (var i = 0; i < symbols.length; i++) {
+    var encodedSymbol = symbols[i].replace("^", "%5E");
+    var url = baseURL + encodedSymbol + queryString;
+    var response = UrlFetchApp.fetch(url);
+    var data = Utilities.parseCsv(response.getContentText());
+    var dates = data.map(function (row) {
+      return [row[0]];
+    });
+    var closingPrices = data.map(function (row) {
+      return [row[4]]; // Измените 4 на индекс колонки с ценой закрытия
+    });
+
+    sheet.getRange(1, column).setValue(labels[i]);
+    sheet.getRange(2, column, dates.length, 1).setValues(dates);
+    sheet.getRange(2, column + 1, closingPrices.length, 1).setValues(closingPrices);
+    column += 2;
+  }
+}
+
+fetchYahooFinanceDataToGoogleSheet();
+
+
+
+
+fetchYahooFinanceDataToGoogleSheet()
+}
+
+
+
+function myFunction() {
+  function updateData() {
+    var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    const sheet = spreadsheet.getSheetByName('Raw Data');
+
+ //   получение данных о дате 
+  let data = sheet.getRange('A8:A').getValues();
+  var dataArray = []
+  let dataCurrencyletter = ["E", "G", "I","K", "M","O","Q","S","U","W","Y","AA","AC","AE","AG","AI","AK","AM","AO","AQ","AS","AU","AW","AY","BA","BC"]
+  let dataHistoricalPrices = ["B","D","F","H","J","L", "N","P","R","T","V","X","Z","AB","AD","AF","AH","AJ","AL","AN","AP","AR","AT","AV","AX","AZ","BB","BD"]
+  let dataRanges = ["Data!A:B", "Data!C:D", "Data!E:F", "Data!G:H", "Data!I:J", "Data!K:L", "Data!M:N", "Data!O:P", "Data!Q:R", "Data!S:T", "Data!U:V", "Data!W:X", "Data!Y:Z", "Data!AA:AB", "Data!AC:AD", "Data!AE:AF", "Data!AG:AH", "Data!AI:AJ", "Data!AK:AL", "Data!AM:AN", "Data!AO:AP", "Data!AQ:AR", "Data!AS:AT", "Data!AU:AV","Data!AW:AX","Data!AY:AZ", "Data!BA:BB", "Data!BC:BD"];
+  var dataRangesEveryOther = ["Data!A:A", "Data!C:C", "Data!E:E", "Data!G:G", "Data!I:I", "Data!K:K", "Data!M:M", "Data!O:O", "Data!Q:Q", "Data!S:S", "Data!U:U", "Data!W:W", "Data!Y:Y", "Data!AA:AA", "Data!AC:AC", "Data!AE:AE", "Data!AG:AG", "Data!AI:AI", "Data!AK:AK", "Data!AM:AM", "Data!AO:AO", "Data!AQ:AQ", "Data!AS:AS", "Data!AU:AU", "Data!AW:AW", "Data!AY:AY", "Data!BA:BA", "Data!BC:BC"];
+  let active = ["USDSPX", "DJI", "DAX", "FTSE", "XAUUSD", "XAUEUR", "XAUGBP", "USDJPY", "EURJPY", "GBPJPY", "CADJPY", "NZDJPY", "AUDJPY", "USDCHF", "EURCHF", "GBPCHF", "CADCHF", "NZDCHF", "AUDCHF", "USDMXN", "USO", "EURHUF", "UNG", "EURUSD", "GBPUSD", "AUDUSD", "NZDUSD", "USDCAD"]
+
+
+
+
+  for (var i = 0; i < data.length; i++) {
+  var value = data[i][0]; // Получение значения из каждой строки
+  if (value !== "") { // Проверка на пустые ячейки
+    dataArray.push(value); // Добавление значения в массив
+  }
+
+  }
+
+
+// Заполнение даты в гугл таблице 
+for(let b=0; b<dataCurrencyletter.length; b++ ){
+  for (var j = 0; j < dataArray.length; j++) {
+    Logger.log('Значение ' + (j + 1) + ': ' + dataArray[j]);
+    sheet.getRange(dataCurrencyletter[b]+ (j +8)).setValue(dataArray[j])
+  }
+}
+
+for(let s=0; s<dataHistoricalPrices.length; s++ ){
+  for(let u=0; u<dataArray.length  ; u++){
+    if (s == 0   ||  s == 1  ||  s == 2   ||  s == 3  ||  s == 4  ||  s == 20 ||  s == 22 ){
+      sheet.getRange(dataHistoricalPrices[s]+ (u +8)).setValue("=iferror (if(A" + (u + 8) + ">0, INDEX(" + dataRanges [s] + ", MATCH(VALUE(A" + (u + 8 )  + "), " + dataRangesEveryOther[s] +",0),2)," + '""),' + dataHistoricalPrices[s]+ (u +8 - 1)  + ")")
+    }
+     else if (s == 5){
+
+      sheet.getRange(dataHistoricalPrices[s]+ (u +8)).setValue(("= J" + (u+ 8) ) + ("/AV"+ (u + 8)  ))
+     }
+     else if (s == 6){
+      sheet.getRange(dataHistoricalPrices[s]+ (u +8)).setValue(("= J" + (u+ 8) ) + ("/AX"+ (u + 8)  ))
+    }
+    else {
+    sheet.getRange(dataHistoricalPrices[s]+ (u +8)).setValue("=iferror (if(A" + (u + 8) + ">0, INDEX(" + dataRanges [s] + ", MATCH(VALUE(A" + (u + 8 + 1)  + "), " + dataRangesEveryOther[s] +",0),2)," + '""),' + dataHistoricalPrices[s]+ (u +8 - 1)  + ")")
+  }
+  }
+}
+
+
+}
+
+updateData()
+
+}
+
+
+function createTable() {
+function updateData() {
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = spreadsheet.getSheetByName('Raw Data');
+
+  /* // получение данных о дате 
+  let data = sheet.getRange('A8:A').getValues();
+  var dataArray = []
+  let dataCurrencyletter = ["E", "G", "I","K", "M","O","Q","S","U","W","Y","AA","AC","AE","AG","AI","AK","AM","AO","AQ","AS","AU","AW","AY","BA","BC"]
+  let dataHistoricalPrices = ["B","D","F","H","J","L", "N","P","R","T","V","X","Z","AB","AD","AF","AH","AJ","AL","AN","AP","AR","AT","AV","AX","AZ","BB","BD"]
+  let dataRanges = ["Data!A:B", "Data!C:D", "Data!E:F", "Data!G:H", "Data!I:J", "Data!K:L", "Data!M:N", "Data!O:P", "Data!Q:R", "Data!S:T", "Data!U:V", "Data!W:X", "Data!Y:Z", "Data!AA:AB", "Data!AC:AD", "Data!AE:AF", "Data!AG:AH", "Data!AI:AJ", "Data!AK:AL", "Data!AM:AN", "Data!AO:AP", "Data!AQ:AR", "Data!AS:AT", "Data!AU:AV","Data!AW:AX","Data!AY:AZ", "Data!BA:BB", "Data!BC:BD"];
+  var dataRangesEveryOther = ["Data!A:A", "Data!C:C", "Data!E:E", "Data!G:G", "Data!I:I", "Data!K:K", "Data!M:M", "Data!O:O", "Data!Q:Q", "Data!S:S", "Data!U:U", "Data!W:W", "Data!Y:Y", "Data!AA:AA", "Data!AC:AC", "Data!AE:AE", "Data!AG:AG", "Data!AI:AI", "Data!AK:AK", "Data!AM:AM", "Data!AO:AO", "Data!AQ:AQ", "Data!AS:AS", "Data!AU:AU", "Data!AW:AW", "Data!AY:AY", "Data!BA:BA", "Data!BC:BC"];
+  let active = ["USDSPX", "DJI", "DAX", "FTSE", "XAUUSD", "XAUEUR", "XAUGBP", "USDJPY", "EURJPY", "GBPJPY", "CADJPY", "NZDJPY", "AUDJPY", "USDCHF", "EURCHF", "GBPCHF", "CADCHF", "NZDCHF", "AUDCHF", "USDMXN", "USO", "EURHUF", "UNG", "EURUSD", "GBPUSD", "AUDUSD", "NZDUSD", "USDCAD"]
+
+  var columnRange = ["BI", "BJ", "BK", "BL", "BM", "BN", "BO", "BP", "BQ", "BR", "BS", "BT", "BU", "BV", "BW", "BX", "BY", "BZ", "CA", "CB", "CC", "CD", "CE", "CF", "CG", "CH", "CI", "CJ"];
+
+      for (var i = 0; i < data.length; i++) {
+  var value = data[i][0]; // Получение значения из каждой строки
+  if (value !== "") { // Проверка на пустые ячейки
+    dataArray.push(value); // Добавление значения в массив
+  }
+
+  }
+
+  for(let s=0; s<dataHistoricalPrices.length; s++ ){
+  for(let u=0; u<dataArray.length -1 ; u++){
+     sheet.getRange(columnRange[s]+ (u +8)).setValue("= ln(" + dataHistoricalPrices[s] + (u + 8) + "/" + dataHistoricalPrices[s] + (u + 9) + ")")
+  }
+  }
+
+}
+
+updateData()
+
+}
+
+
+
+function delateData() {
+function updateData() {
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = spreadsheet.getSheetByName('Raw Data');
+
+   // /* получение данных о дате 
+  let data = sheet.getRange('A8:A').getValues();
+  var dataArray = []
+  let dataCurrencyletter = ["E", "G", "I","K", "M","O","Q","S","U","W","Y","AA","AC","AE","AG","AI","AK","AM","AO","AQ","AS","AU","AW","AY","BA","BC"]
+  let dataHistoricalPrices = ["B","D","F","H","J","L", "N","P","R","T","V","X","Z","AB","AD","AF","AH","AJ","AL","AN","AP","AR","AT","AV","AX","AZ","BB","BD"]
+  let dataRanges = ["Data!A:B", "Data!C:D", "Data!E:F", "Data!G:H", "Data!I:J", "Data!K:L", "Data!M:N", "Data!O:P", "Data!Q:R", "Data!S:T", "Data!U:V", "Data!W:X", "Data!Y:Z", "Data!AA:AB", "Data!AC:AD", "Data!AE:AF", "Data!AG:AH", "Data!AI:AJ", "Data!AK:AL", "Data!AM:AN", "Data!AO:AP", "Data!AQ:AR", "Data!AS:AT", "Data!AU:AV","Data!AW:AX","Data!AY:AZ", "Data!BA:BB", "Data!BC:BD"];
+  var dataRangesEveryOther = ["Data!A:A", "Data!C:C", "Data!E:E", "Data!G:G", "Data!I:I", "Data!K:K", "Data!M:M", "Data!O:O", "Data!Q:Q", "Data!S:S", "Data!U:U", "Data!W:W", "Data!Y:Y", "Data!AA:AA", "Data!AC:AC", "Data!AE:AE", "Data!AG:AG", "Data!AI:AI", "Data!AK:AK", "Data!AM:AM", "Data!AO:AO", "Data!AQ:AQ", "Data!AS:AS", "Data!AU:AU", "Data!AW:AW", "Data!AY:AY", "Data!BA:BA", "Data!BC:BC"];
+  let active = ["USDSPX", "DJI", "DAX", "FTSE", "XAUUSD", "XAUEUR", "XAUGBP", "USDJPY", "EURJPY", "GBPJPY", "CADJPY", "NZDJPY", "AUDJPY", "USDCHF", "EURCHF", "GBPCHF", "CADCHF", "NZDCHF", "AUDCHF", "USDMXN", "USO", "EURHUF", "UNG", "EURUSD", "GBPUSD", "AUDUSD", "NZDUSD", "USDCAD"]
+var columnRange = ["BI", "BJ", "BK", "BL", "BM", "BN", "BO", "BP", "BQ", "BR", "BS", "BT", "BU", "BV", "BW", "BX", "BY", "BZ", "CA", "CB", "CC", "CD", "CE", "CF", "CG", "CH", "CI", "CJ"];
+
+
+
+  for (var i = 0; i < data.length; i++) {
+  var value = data[i][0]; // Получение значения из каждой строки
+  if (value !== "") { // Проверка на пустые ячейки
+    dataArray.push(value); // Добавление значения в массив
+  }
+
+  }
+  
+
+// Заполнение даты в гугл таблице 
+for(let b=0; b<dataCurrencyletter.length; b++ ){
+  for (var j = 0; j < dataArray.length; j++) {
+    Logger.log('Значение ' + (j + 1) + ': ' + dataArray[j]);
+    sheet.getRange(dataCurrencyletter[b]+ (j +8)).setValue("")
+  }
+}
+
+for(let s=0; s<dataHistoricalPrices.length; s++ ){
+  for(let u=0; u<dataArray.length  ; u++){
+     
+    sheet.getRange(dataHistoricalPrices[s]+ (u +8)).setValue("")
+  
+  }
+}
+
+    for(let m=0; m<dataHistoricalPrices.length; m++ ){
+  for(let f=0; f<dataArray.length -1 ; f++){
+     sheet.getRange(columnRange[m]+ (f +9)).setValue("")
+  }
+  }
+
+
+}
+
+updateData()
+
+}
+
+
+
+  let dataCurrencyletter = ["E", "G", "I","K", "M","O","Q","S","U","W","Y","AA","AC","AE","AG","AI","AK","AM","AO","AQ","AS","AU","AW","AY","BA","BC"]
+function matrix() {
+function updateData() {
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = spreadsheet.getSheetByName('Raw Data');
+
+// /* получение данных о дате 
+  let data = sheet.getRange('A8:A').getValues();
+  var dataArray = []
+  let dataHistoricalPrices = ["B","D","F","H","J","L", "N","P","R","T","V","X","Z","AB","AD","AF","AH","AJ","AL","AN","AP","AR","AT","AV","AX","AZ","BB","BD"]
+  let dataRanges = ["Data!A:B", "Data!C:D", "Data!E:F", "Data!G:H", "Data!I:J", "Data!K:L", "Data!M:N", "Data!O:P", "Data!Q:R", "Data!S:T", "Data!U:V", "Data!W:X", "Data!Y:Z", "Data!AA:AB", "Data!AC:AD", "Data!AE:AF", "Data!AG:AH", "Data!AI:AJ", "Data!AK:AL", "Data!AM:AN", "Data!AO:AP", "Data!AQ:AR", "Data!AS:AT", "Data!AU:AV","Data!AW:AX","Data!AY:AZ", "Data!BA:BB", "Data!BC:BD"];
+  var dataRangesEveryOther = ["Data!A:A", "Data!C:C", "Data!E:E", "Data!G:G", "Data!I:I", "Data!K:K", "Data!M:M", "Data!O:O", "Data!Q:Q", "Data!S:S", "Data!U:U", "Data!W:W", "Data!Y:Y", "Data!AA:AA", "Data!AC:AC", "Data!AE:AE", "Data!AG:AG", "Data!AI:AI", "Data!AK:AK", "Data!AM:AM", "Data!AO:AO", "Data!AQ:AQ", "Data!AS:AS", "Data!AU:AU", "Data!AW:AW", "Data!AY:AY", "Data!BA:BA", "Data!BC:BC"];
+  let active = ["USDSPX", "DJI", "DAX", "FTSE", "XAUUSD", "XAUEUR", "XAUGBP", "USDJPY", "EURJPY", "GBPJPY", "CADJPY", "NZDJPY", "AUDJPY", "USDCHF", "EURCHF", "GBPCHF", "CADCHF", "NZDCHF", "AUDCHF", "USDMXN", "USO", "EURHUF", "UNG", "EURUSD", "GBPUSD", "AUDUSD", "NZDUSD", "USDCAD"]
+var columnRange = ["BI", "BJ", "BK", "BL", "BM", "BN", "BO", "BP", "BQ", "BR", "BS", "BT", "BU", "BV", "BW", "BX", "BY", "BZ", "CA", "CB", "CC", "CD", "CE", "CF", "CG", "CH", "CI", "CJ"];
+
+
+  sheet.getRange("CN8").setValue(dataCurrencyletter[1])
+ 
+
+
+}
+
+updateData()
+
+}
+
+
+*/
+
